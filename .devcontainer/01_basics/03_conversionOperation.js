97e16d1d831684ac2723e29c8a60cdf4fd4ae4e1
Sameer Pathan <116096924+Sameer26318@users.noreply.github.com>
2024-01-13 11:58:21 +0000

    conversion of dataTypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Sameer"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a Number)
+// true => 1,false => 0
+
+let isloggedIn ="Sameer"
+
+let booleanIsLoggedIn = Boolean(isloggedIn) 
+//console.log(booleanIsLoggedIn);
+
+// 1 => true , 0 =>false
+// "" => false
+// "Sameer" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
