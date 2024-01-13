855031abae6ba3c1dfe805dbe1bef7be6879f0d2
Sameer Pathan <116096924+Sameer26318@users.noreply.github.com>
2024-01-13 12:30:22 +0000

    conversion operation 2

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,5 +24,45 @@ let booleanIsLoggedIn = Boolean(isloggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// *******************Operations*******************
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/2);
+//console.log(2%2);
+
+let str1 = "hello"
+let str2 = "sameer"
+
+let str3 = str1 + str2
+//console.log(str3);
+
+//console.log("1" + 2);
+//console.log(1 + 2 + "2");
+//console.log("1" + 2 + 2);
+//console.log(1 + 2 + "2");
+
+//console.log((3 + 4) *3 % 5);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study 
+https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
