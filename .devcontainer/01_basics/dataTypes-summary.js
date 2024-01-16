c423f7f1e3cdea829642e6dfea3a30059c67f080
Sameer Pathan <116096924+Sameer26318@users.noreply.github.com>
2024-01-16 15:07:23 +0000

    dataType summary done

---
+++ .devcontainer/01_basics/dataTypes-summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+//const bigNumber = 356235434774n
+
+
+
+// Refrence (Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "Sameer",
+    age: 23,
+}
+
+const myFunction = function(){
+ console.log("Hello World");
+}
+
+console.log(typeof outsideTemp);
